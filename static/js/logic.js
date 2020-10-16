6a3ebc355801b49f60dc8c431ff4539b8687a393
arhunt <allan.r.hunt@gmail.com>
2020-10-16 15:37:53 -0500

    Comments and cleanup.

--- static/js/logic.js
+++ static/js/logic.js
@@ -74,6 +74,7 @@ function createMap(earthquakes){
     accessToken: API_KEY
     });
 
+    // Satellite map layer
     var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -117,7 +118,7 @@ function createMap(earthquakes){
 
     // Legend - code from https://leafletjs.com/examples/choropleth/
     var legend = L.control({position: "bottomright"});
-    
+
     legend.onAdd = function(map) {
         var div = L.DomUtil.create("div", "info legend"),
         grades = [],
